1953b90e10c7aec328cf0b2a1f2abb32f5605ec3
연성민 <ysm1180@gmail.com>
2019-05-02 16:24:57 +0900

    Raise error when lol api error

--- main/utility/api.py
+++ main/utility/api.py
@@ -7,6 +7,7 @@ from django.conf import settings
 from main.settings import LOL_API, LOL_URL
 from main.utility.redis_client import REDIS_CONNECTION_POOL
 
+
 def get_lol_last_version():
     r = redis.Redis(connection_pool=REDIS_CONNECTION_POOL)
     version = r.get('VERSION')
@@ -19,62 +20,51 @@ def get_lol_last_version():
             r.set('VERSION', version.encode('utf-8'), 86400)
 
         if version is None:
-            raise ValueError('version data does not exist.')
+            response.raise_for_status()
     else:
         version = version.decode('utf-8')
-    
+
     return version
 
 
-def call_lol_api(url, additional_params={}):
-    params = {'api_key': settings.LOL_API_KEY}
-    params.update(additional_params)
-    response = requests.get(url, params=params)
-    return response
+def call_lol_api(url, params={}):
+    headers = {'X-Riot-Token': settings.LOL_API_KEY}
+    response = requests.get(url, params=params, headers=headers)
+    response.raise_for_status()
+    return response.json()
 
 
 def call_summoner_api_by_account_id(account_id):
     url = (LOL_API['GET_SUMMONER_BY_ACCOUNT_ID'] % quote(account_id))
-    response = call_lol_api(url)
-    if response.status_code == 404:
-        return None
-
-    return response.json()
+    data = call_lol_api(url)
+    return data
 
 
 def call_summoner_api_by_id(id):
     url = (LOL_API['GET_SUMMONER_BY_ID'] % quote(id))
-    response = call_lol_api(url)
-    if response.status_code == 404:
-        return None
-
-    return response.json()
+    data = call_lol_api(url)
+    return data
 
 
 def call_summoner_api_by_name(name):
     url = (LOL_API['GET_SUMMONER_BY_NAME'] % quote(name))
-    response = call_lol_api(url)
-    if response.status_code == 404:
-        return None
-
-    return response.json()
+    data = call_lol_api(url)
+    return data
 
 
 def call_champion_masteries_api_by_id(encrypted_id):
     url = (LOL_API['GET_CHAMPION_MASTERIES'] % quote(encrypted_id))
-    response = call_lol_api(url)
-
-    if response.status_code == 404:
-        return None
-
-    return response.json()
+    data = call_lol_api(url)
+    return data
 
 
 def call_match_list_api_by_account_id(account_id, start, end):
     url = (LOL_API['GET_MATCH_LIST_BY_ACCOUNT_ID'] % quote(account_id))
-    response = call_lol_api(url, {'beginIndex': start, 'endIndex': end})
+    data = call_lol_api(url, {'beginIndex': start, 'endIndex': end})
+    return data
 
-    if response.status_code == 404:
-        return None
 
-    return response.json()
+def call_match_info_api_by_game_id(game_id):
+    url = (LOL_API['GET_MATCH_INFO_BY_GAME_ID'] % game_id)
+    data = call_lol_api(url)
+    return data

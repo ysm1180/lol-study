b24b24043cf4eb39be3a16f12185d5d25f49315b
연성민 <ysm1180@gmail.com>
2019-05-02 17:14:36 +0900

    Implements match game data

--- main/models.py
+++ main/models.py
@@ -1,3 +1,6 @@
+# -*- coding:utf-8 -*-
+
+import math
 import time
 from datetime import datetime
 
@@ -37,6 +40,7 @@ class Summoner(models.Model):
     puuid = models.CharField(max_length=78)
     level = models.IntegerField(default=0)
     icon_id = models.IntegerField(default=0)
+    updated_time = models.DateTimeField(auto_now=True)
 
     class Meta:
         app_label = 'main'
@@ -123,6 +127,8 @@ class Match(models.Model):
             queue_type = '자유랭'
 
         return {
+            'game_id':
+            self.game_id,
             'champion_name':
             champion_info['name'],
             'champion_icon_url':
@@ -139,4 +145,116 @@ class Match(models.Model):
             self.role,
             'lane':
             self.lane,
+        }
+
+
+class Game(models.Model):
+    game_id = models.BigIntegerField(primary_key=True)
+    season = models.IntegerField(default=0)
+    queue = models.IntegerField(default=0)
+    platform = models.CharField(max_length=2)
+    duration = models.BigIntegerField(default=0)
+    type = models.CharField(max_length=32)
+    mode = models.CharField(max_length=32)
+    map_id = models.IntegerField(default=0)
+
+    # participants = ArrayField(JSONField())
+
+    class Meta:
+        app_label = 'main'
+
+    def get_client_data(self):
+        duration_format = '%d분 %d초' % (math.floor(
+            self.duration / 60), self.duration % 60)
+
+        return {
+            'id': self.game_id,
+            'duration': duration_format,
+        }
+
+
+class GameTeam(models.Model):
+    game = models.ForeignKey(Game,
+                             to_field='game_id',
+                             on_delete=models.CASCADE)
+    team_id = models.IntegerField(default=0)
+    is_win = models.BooleanField(default=False)
+    tower_kills = models.IntegerField(default=0)
+
+    class Meta:
+        app_label = 'main'
+        unique_together = (('game', 'team_id'), )
+
+    def get_client_data(self):
+        return {
+            'game_id': self.game_id,
+            'team_id': self.team_id,
+            'is_win': self.is_win,
+            'tower_kills': self.tower_kills,
+        }
+
+
+class GameParticipant(models.Model):
+    game = models.ForeignKey(Game,
+                             to_field='game_id',
+                             on_delete=models.CASCADE)
+    participant_id = models.IntegerField()
+    summoner_id = models.CharField(max_length=63, db_index=True)
+    display_summoner_name = models.CharField(max_length=64, db_index=True)
+    team_id = models.IntegerField(default=0)
+    champion = models.ForeignKey(Champion, on_delete=models.CASCADE)
+    spell_id_1 = models.ForeignKey(Spell,
+                                   related_name='spells_1',
+                                   on_delete=models.CASCADE)
+    spell_id_2 = models.ForeignKey(Spell,
+                                   related_name='spells_2',
+                                   on_delete=models.CASCADE)
+    item_0 = models.IntegerField(default=0)
+    item_1 = models.IntegerField(default=0)
+    item_2 = models.IntegerField(default=0)
+    item_3 = models.IntegerField(default=0)
+    item_4 = models.IntegerField(default=0)
+    item_5 = models.IntegerField(default=0)
+    item_6 = models.IntegerField(default=0)
+    kills = models.IntegerField(default=0)
+    deaths = models.IntegerField(default=0)
+    assists = models.IntegerField(default=0)
+    champ_level = models.IntegerField(default=0)
+
+    class Meta:
+        app_label = 'main'
+        unique_together = (('game', 'participant_id'), )
+
+    def get_client_data(self):
+        version = get_lol_last_version()
+        champion_info = load_champion_info(self.champion_id)
+
+        return {
+            'game_id':
+            self.game_id,
+            'team_id':
+            self.team_id,
+            'summoner_name':
+            self.display_summoner_name,
+            'champion_name':
+            champion_info['name'],
+            'champion_icon_url':
+            (LOL_URL['CHAMPION_ICON'] % (version, champion_info['id'])),
+            'champion_level':
+            self.champ_level,
+            'spell_icon_url':
+            (LOL_URL['SPELL_ICON'] % (version, champion_info['id'])),
+            'item_0_icon_url': (LOL_URL['ITEM_ICON'] % (version, self.item_0)),
+            'item_1_icon_url': (LOL_URL['ITEM_ICON'] % (version, self.item_1)),
+            'item_2_icon_url': (LOL_URL['ITEM_ICON'] % (version, self.item_2)),
+            'item_3_icon_url': (LOL_URL['ITEM_ICON'] % (version, self.item_3)),
+            'item_4_icon_url': (LOL_URL['ITEM_ICON'] % (version, self.item_4)),
+            'item_5_icon_url': (LOL_URL['ITEM_ICON'] % (version, self.item_5)),
+            'item_6_icon_url': (LOL_URL['ITEM_ICON'] % (version, self.item_6)),
+            'kills':
+            self.kills,
+            'deaths':
+            self.deaths,
+            'assists':
+            self.assists,
         }
--- main/views.py
+++ main/views.py
@@ -1,16 +1,18 @@
 # -*- coding:utf-8 -*-
 
+import requests
 from django.conf import settings
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, render_to_response
 from django.urls import reverse
 
-from main.models import Champion, ChampionMastery, Match, Summoner
-from main.utility.api import (call_champion_masteries_api_by_id,
-                              call_match_list_api_by_account_id,
-                              call_summoner_api_by_account_id,
-                              call_summoner_api_by_id,
-                              call_summoner_api_by_name)
+from .models import (Champion, ChampionMastery, Game, GameParticipant,
+                     GameTeam, Match, Summoner, Spell)
+from .utility.api import (call_champion_masteries_api_by_id,
+                          call_match_info_api_by_game_id,
+                          call_match_list_api_by_account_id,
+                          call_summoner_api_by_account_id,
+                          call_summoner_api_by_id, call_summoner_api_by_name)
 
 
 def index(request):
@@ -18,6 +20,132 @@ def index(request):
 
 
 cache_champion_models = {}
+cache_spell_models = {}
+
+
+def get_game_infos(game_ids):
+    games = Game.objects.filter(game_id__in=game_ids)
+    result_game_ids = list(map(lambda game_model: game_model.game_id, games))
+    invalid_game_ids = list(set(game_ids) - set(result_game_ids))
+
+    game_models = []
+    game_team_models = []
+    game_participant_models = []
+    if len(invalid_game_ids) > 0:
+        for game_id in invalid_game_ids:
+            game_data = call_match_info_api_by_game_id(game_id)
+            game_model = Game(game_id=game_id,
+                              season=game_data['seasonId'],
+                              queue=game_data['queueId'],
+                              platform=game_data['platformId'],
+                              duration=game_data['gameDuration'],
+                              type=game_data['gameType'],
+                              mode=game_data['gameMode'],
+                              map_id=game_data['mapId'])
+            game_models.append(game_model)
+
+            for team in game_data['teams']:
+                is_win = False
+                if team['win'] == 'Win':
+                    is_win = True
+
+                game_team_model = GameTeam(game=game_model,
+                                           team_id=team['teamId'],
+                                           is_win=is_win,
+                                           tower_kills=team['towerKills'])
+                game_team_models.append(game_team_model)
+
+            participants_data = {}
+            for participant in game_data['participants']:
+                participants_data[participant['participantId']] = participant
+
+            summoner_ids = []
+            for identity in game_data['participantIdentities']:
+                summoner_ids.append(identity['player']['summonerId'])
+            summoner_list = Summoner.objects.filter(pk__in=summoner_ids)
+            result_summoner_ids = list(
+                map(lambda summoner_model: summoner_model.encrypted_id,
+                    summoner_list))
+
+            for identity in game_data['participantIdentities']:
+                summoner_id = identity['player']['summonerId']
+                summoner_name = identity['player']['summonerName']
+                if summoner_id in result_summoner_ids:
+                    summoner_name = [
+                        summoner_model.name for summoner_model in summoner_list
+                        if summoner_model.encrypted_id == summoner_id
+                    ][0]
+
+                participant_info = participants_data[identity['participantId']]
+
+                try:
+                    if participant_info[
+                            'championId'] in cache_champion_models.keys():
+                        champion_model = cache_champion_models[
+                            participant_info['championId']]
+                    else:
+                        champion_model = Champion.objects.get(
+                            pk=participant_info['championId'])
+                        cache_champion_models[
+                            participant_info['championId']] = champion_model
+                except Champion.DoesNotExist:
+                    raise
+
+                try:
+                    if participant_info['spell1Id'] in cache_spell_models.keys(
+                    ):
+                        spell_1_model = cache_spell_models[
+                            participant_info['spell1Id']]
+                    else:
+                        spell_1_model = Spell.objects.get(
+                            pk=participant_info['spell1Id'])
+                        cache_spell_models[
+                            participant_info['spell1Id']] = spell_1_model
+
+                    if participant_info['spell2Id'] in cache_spell_models.keys(
+                    ):
+                        spell_2_model = cache_spell_models[
+                            participant_info['spell2Id']]
+                    else:
+                        spell_2_model = Spell.objects.get(
+                            pk=participant_info['spell2Id'])
+                        cache_spell_models[
+                            participant_info['spell2Id']] = spell_2_model
+                except Spell.DoesNotExist:
+                    raise
+
+                game_participant_model = GameParticipant(
+                    game=game_model,
+                    participant_id=identity['participantId'],
+                    summoner_id=summoner_id,
+                    display_summoner_name=summoner_name,
+                    team_id=participant_info['teamId'],
+                    champion=champion_model,
+                    spell_id_1=spell_1_model,
+                    spell_id_2=spell_2_model,
+                    item_0=participant_info['stats']['item0'],
+                    item_1=participant_info['stats']['item1'],
+                    item_2=participant_info['stats']['item2'],
+                    item_3=participant_info['stats']['item3'],
+                    item_4=participant_info['stats']['item4'],
+                    item_5=participant_info['stats']['item5'],
+                    item_6=participant_info['stats']['item6'],
+                    kills=participant_info['stats']['kills'],
+                    deaths=participant_info['stats']['deaths'],
+                    assists=participant_info['stats']['assists'],
+                    champ_level=participant_info['stats']['champLevel'])
+                game_participant_models.append(game_participant_model)
+
+        Game.objects.bulk_create(game_models)
+        GameTeam.objects.bulk_create(game_team_models)
+        GameParticipant.objects.bulk_create(game_participant_models)
+
+    games = Game.objects.filter(game_id__in=game_ids)
+    game_teams = GameTeam.objects.filter(game_id__in=game_ids)
+    game_participants = GameParticipant.objects.filter(game_id__in=game_ids)
+
+    return games, game_teams, game_participants
+
 
 def get_champion_match_list(account_id, start, end):
     matches = Match.objects.filter(
@@ -50,7 +178,7 @@ def get_champion_match_list(account_id, start, end):
             matches.append(match_model)
         Match.objects.bulk_create(matches)
 
-    return matches
+    return matches, list(map(lambda match: match.game_id, matches))
 
 
 def get_champion_mastery(id):
@@ -144,11 +272,11 @@ def get_summoner(request):
                       {'error_message': "정상적인 접근이 아닙니다."})
 
     try:
-    summoner = get_summoner_data_by_name(name)
+        summoner = get_summoner_data_by_name(name)
     except requests.exceptions.HTTPError as e:
         if e.response.status_code == 404:
             render(request, 'main/index.html',
-                      {'error_message': "등록되지 않은 소환사입니다."})
+                   {'error_message': "등록되지 않은 소환사입니다."})
         else:
             render(request, 'main/index.html', {'error_message': e.msg})
 
@@ -157,31 +285,68 @@ def get_summoner(request):
 
 def summoner(request, name):
     try:
-    summoner_model = get_summoner_data_by_name(name)
+        summoner_model = get_summoner_data_by_name(name)
 
-    if len(cache_champion_models) == 0:
-        champions = Champion.objects.all()
-        for champion in champions:
-            cache_champion_models[champion.key] = champion
+        if len(cache_champion_models) == 0:
+            champions = Champion.objects.all()
+            for champion in champions:
+                cache_champion_models[champion.key] = champion
 
-    champion_mastery_model_list = get_champion_mastery(
-        summoner_model.encrypted_id)
-    if champion_mastery_model_list is None:
-        return render(request, 'main/index.html',
-                      {'error_message': "정보를 불러오는데에 문제가 생겼습니다."})
+        if len(cache_spell_models) == 0:
+            spells = Spell.objects.all()
+            for spell in spells:
+                cache_spell_models[spell.key] = spell
 
-    match_model_list = get_champion_match_list(summoner_model.account_id, 0,
-                                               20)
+        champion_mastery_model_list = get_champion_mastery(
+            summoner_model.encrypted_id)
 
-    matches = []
-    for match_model in match_model_list:
-        matches.append(match_model.get_client_data())
+        match_model_list, game_ids = get_champion_match_list(
+            summoner_model.account_id, 0, 20)
+        game_models, team_models, participant_models = get_game_infos(game_ids)
 
-    summoner_client_data = summoner_model.get_client_data()
-    return render(request, 'main/summoner.html', {
-        'summoner_data': summoner_client_data,
-        'match_list': matches,
-    })
+        games = {}
+        for game_model in game_models:
+            games[game_model.game_id] = {
+                'game': game_model.get_client_data(),
+                'teams': {},
+                'participants': {},
+            }
+
+        for team_model in team_models:
+            games[team_model.game_id]['teams'][
+                team_model.team_id] = team_model.get_client_data()
+
+        summoner_game_data = {}
+        for participant_model in participant_models:
+            games[participant_model.game_id]['participants'][
+                participant_model.
+                participant_id] = participant_model.get_client_data()
+
+            if summoner_model.encrypted_id == participant_model.summoner_id:
+                summoner_game = {'team': {}, 'participant': {}}
+                summoner_game['team'] = games[
+                    participant_model.game_id]['teams'][
+                        participant_model.team_id]
+                summoner_game[
+                    'participant'] = participant_model.get_client_data()
+                summoner_game_data[participant_model.game_id] = summoner_game
+
+        matches = []
+        for match_model in match_model_list:
+            match_data = match_model.get_client_data()
+            match_data.update({
+                'game':
+                games[match_data['game_id']],
+                'summoner_game':
+                summoner_game_data[match_data['game_id']],
+            })
+            matches.append(match_data)
+
+        summoner_client_data = summoner_model.get_client_data()
+        return render(request, 'main/summoner.html', {
+            'summoner_data': summoner_client_data,
+            'match_list': matches,
+        })
 
     except requests.exceptions.HTTPError as e:
         if e.response.status_code == 404:

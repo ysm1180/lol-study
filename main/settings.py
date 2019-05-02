b894708689e372ba36cd936c50ed39db0997052b
연성민 <ysm1180@gmail.com>
2019-05-02 16:23:47 +0900

    Adds static url

--- main/settings.py
+++ main/settings.py
@@ -15,18 +15,23 @@ LOL_URL = {
     'http://ddragon.leagueoflegends.com/cdn/%s/data/ko_KR/champion/%s.json',
     'CHAMPION_ICON':
     'http://ddragon.leagueoflegends.com/cdn/%s/img/champion/%s.png',
+    'STATIC_SPELL_ALL_DATA':
+    'http://ddragon.leagueoflegends.com/cdn/%s/data/ko_KR/summoner.json',
+    'SPELL_ICON':
+    'http://ddragon.leagueoflegends.com/cdn/%s/img/spell/%s.png',
+    'ITEM_ICON':
+    'http://ddragon.leagueoflegends.com/cdn/%s/img/item/%d.png',
 }
 
 LOL_API = {
     'GET_SUMMONER_BY_ACCOUNT_ID':
     BASE_API_URL + '/lol/summoner/v4/summoners/by-account/%s',
-    'GET_SUMMONER_BY_ID':
-    BASE_API_URL + '/lol/summoner/v4/summoners/%s',
+    'GET_SUMMONER_BY_ID': BASE_API_URL + '/lol/summoner/v4/summoners/%s',
     'GET_SUMMONER_BY_NAME':
     BASE_API_URL + '/lol/summoner/v4/summoners/by-name/%s',
-    'GET_CHAMPION_MASTERIES':
-    BASE_API_URL +
+    'GET_CHAMPION_MASTERIES': BASE_API_URL +
     '/lol/champion-mastery/v4/champion-masteries/by-summoner/%s',
     'GET_MATCH_LIST_BY_ACCOUNT_ID':
     BASE_API_URL + '/lol/match/v4/matchlists/by-account/%s',
+    'GET_MATCH_INFO_BY_GAME_ID': BASE_API_URL + '/lol/match/v4/matches/%d',
 }
